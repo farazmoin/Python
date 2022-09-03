0d7f698661208a20f7aa68ea613907d920d4126a
Faraz M <farazmoin.uk@gmail.com>
2022-09-03 16:54:19 -0400

    modified

--- MAC_Changer/mac_changer.py
+++ MAC_Changer/mac_changer.py
@@ -1,19 +1,24 @@
 import subprocess
 import argparse
 
+
+### Secure ###
+def mac_changer(interface, new_mac):
+    print('Changing MAC for ', interface, 'to ', new_mac)
+
+    subprocess.call(['ifconfig', interface, 'down'])
+    subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
+    subprocess.call(['ifconfig', interface, 'up'])
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--interface', dest='interface', help='Interface to change the MAC of')
 parser.add_argument('-m', '--newMac', dest='new_mac', help='New MAC address')
 args = vars(parser.parse_args())
 
-### Should you wish to take user-input separately, uncomment the 2 lines below ###
+### Should you wish to take user-input separately for interface, MAC, uncomment the 2 lines below ###
 
 # interface = input('Enter the interface name: ')
 # new_mac = input('Enter the new MAC address: ')
-
-interface = args['interface']
-new_mac = args['new_mac']
-print('Changing MAC for ', interface, 'to ', new_mac)
 
 ### Insecure ###
 
@@ -21,10 +26,4 @@ print('Changing MAC for ', interface, 'to ', new_mac)
 # subprocess.call(f'ifconfig {interface} hw ether {new_mac}', shell=True)
 # subprocess.call(f'ifconfig {interface} up', shell=True)
 
-### Secure ###
-
-subprocess.call(['ifconfig', interface, 'down'])
-subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
-subprocess.call(['ifconfig', interface, 'up'])
-
-
+mac_changer(args['interface'], args['new_mac'])

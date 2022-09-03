0847478afe6c42e2d622d15eeaa95ffd166598f3
Faraz <farazmoin.uk@gmail.com>
2022-09-03 17:31:45 +0100

    modified

--- MAC_Changer/mac_changer.py
+++ MAC_Changer/mac_changer.py
@@ -1,5 +1,10 @@
 import subprocess
 
+interface = 'eth0'
+new_mac = '00:11:22:33:44:66'
+
+print('Chaning MAC for ', interface, 'to ', new_mac)
+
 subprocess.call('ifconfig eth0 down', shell=True)
 subprocess.call('ifconfig eth0 hw ether 00:11:22:33:44:55', shell=True)
 subprocess.call('ifconfig eth0 up', shell=True)

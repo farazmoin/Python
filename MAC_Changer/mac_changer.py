cf1f0a20dd141d8ded6750e1cd459b859267c529
Faraz M <farazmoin.uk@gmail.com>
2022-09-03 17:11:52 -0400

    modified

--- MAC_Changer/mac_changer.py
+++ MAC_Changer/mac_changer.py
@@ -1,6 +1,12 @@
 import subprocess
 import argparse
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--interface', dest='interface', help='Interface to change the MAC of')
+    parser.add_argument('-m', '--newMac', dest='new_mac', help='New MAC address')
+    return vars(parser.parse_args())
+
 ### Secure ###
 def mac_changer(interface, new_mac):
     print('Changing MAC for ', interface, 'to ', new_mac)
@@ -15,14 +21,10 @@ def mac_changer(interface, new_mac):
 # subprocess.call(f'ifconfig {interface} hw ether {new_mac}', shell=True)
 # subprocess.call(f'ifconfig {interface} up', shell=True)
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--interface', dest='interface', help='Interface to change the MAC of')
-parser.add_argument('-m', '--newMac', dest='new_mac', help='New MAC address')
-args = vars(parser.parse_args())
-
 ### Should you wish to take user-input separately for interface, MAC, uncomment the 2 lines below ###
 
 # interface = input('Enter the interface name: ')
 # new_mac = input('Enter the new MAC address: ')
 
+args = get_args()
 mac_changer(args['interface'], args['new_mac'])

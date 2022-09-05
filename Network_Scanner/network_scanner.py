3fbdc742733729a4eedb49c8fa7edba70d35a6b1
Faraz M <farazmoin.uk@gmail.com>
2022-09-05 11:52:42 -0400

    modified

--- Network_Scanner/network_scanner.py
+++ Network_Scanner/network_scanner.py
@@ -5,8 +5,9 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
     broadcast_arp_request = broadcast/arp_request
-    answered, unanswered = scapy.srp(broadcast_arp_request, timeout=1)
-    print(answered.summary())
-
+    answered = scapy.srp(broadcast_arp_request, timeout=1, verbose=False)[0]
+    print('IP\t\t\tMAC\n++++++++++++')
+    for i in answered:
+        print(i[1].psrc, '\t', i[1].hwsrc)
 
 scan('192.168.160.1/24')

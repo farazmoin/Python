168bcdfcd1b41cce3b25488d7e9ec4a76cb51229
Faraz M <farazmoin.uk@gmail.com>
2022-09-06 14:14:13 -0400

    modified

--- Network_Scanner/network_scanner.py
+++ Network_Scanner/network_scanner.py
@@ -1,4 +1,12 @@
 import scapy.all as scapy
+import argparse
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-ip', '--ipaddress', help='IP address or range', dest='ip')
+    args = vars(parser.parse_args())
+    return args
 
 
 def scan(ip):
@@ -6,8 +14,21 @@ def scan(ip):
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
     broadcast_arp_request = broadcast/arp_request
     answered = scapy.srp(broadcast_arp_request, timeout=1, verbose=False)[0]
-    print('IP\t\t\tMAC\n++++++++++++')
-    for i in answered:
-        print(i[1].psrc, '\t', i[1].hwsrc)
 
-scan('192.168.160.1/24')
+    clients_list = list()
+    for i in answered:
+        client_dictionary = {'ip': i[1].psrc, 'mac': i[1].hwsrc}
+        clients_list.append(client_dictionary)
+        # print(i[1].psrc, '\t', i[1].hwsrc)
+    return clients_list
+
+
+def print_results(lst):
+    print('IP\t\t\tMAC\n__________________________________')
+    for client in lst:
+        print(client['ip'], '\t', client['mac'])
+
+
+args = get_args()
+lst = scan(args['ip'])
+print_results(lst)

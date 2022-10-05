d82690e9da3ada1b7475651b796431c11d97ac65
Faraz <farazmoin.uk@gmail.com>
2022-10-05 04:10:26 +0100

    'treasure_island'

---
+++ 100_days_of_code/treasure_island.py
@@ -0,0 +1,48 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#Write your code below this line 👇
+
+direction = input('Let us start. Do you want to go left or right?')
+direction = direction.lower()
+
+if direction == 'left':
+  decision = input('Whoa! You have ended up in water. Do you want to swim or wait?')
+  decision = decision.lower()
+  if decision == 'wait':
+    color = input('Your wait paid off well!, you are now in a room. Which color door will you open? Red, Blue, or Yellow')
+    color = color.lower()
+    if color == 'yellow':
+      print('You Win!')
+    elif color == 'red':
+      print('You were burned by fire. Game Over!')
+    elif color == 'blue':
+      print('You were eaten by beasts. Game Over!')
+    else:
+      print('Game Over!')
+  else:
+    print('You were attacked by a shark. Game Over!')
+else:
+  print('You fell into a hole. Game Over!')
